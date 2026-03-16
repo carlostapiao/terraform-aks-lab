6af562cc5ce48fde080ce9c26db2054d34e511ba
Carlos Tapia <carlos.tapia@tecsup.edu.pe>
2026-03-16 18:56:30 -0500

    infra: aks con terraform

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdns"
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_B2ps_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-k8s-lab1"
+}
+
+variable "location" {
+  default = "centralus"
+}
+
+variable "aks_name" {
+  default = "aks-lab"
+}
